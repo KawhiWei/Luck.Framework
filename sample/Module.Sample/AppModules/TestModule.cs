c3bfeabbfc6626f2478848cebc6d1af3bc60b612
泽威 王 <1585955375@qq.com>
2022-04-20 22:41:34 +0800

    feat : 添加模块化测试项目

---
+++ sample/Module.Sample/AppModules/TestModule.cs
@@ -0,0 +1,15 @@
+﻿using Luck.Framework.Infrastructure;
+
+namespace Module.Sample
+{
+    public class TestModule : AppModule
+    {
+
+
+        public override void ConfigureServices(ConfigureServicesContext context)
+        {
+            base.ConfigureServices(context);
+        }
+
+    }
+}
---
+++ sample/Module.Sample/AppWebModule.cs
@@ -0,0 +1,17 @@
+﻿using Luck.Framework.Infrastructure;
+
+namespace Module.Sample
+{
+    [DependsOn(
+        typeof(DependencyAppModule),
+        typeof(TestModule)
+    )]
+    public class AppWebModule: AppModule
+    {
+        public override void ConfigureServices(ConfigureServicesContext context)
+        {
+
+            base.ConfigureServices(context);
+        }
+    }
+}
