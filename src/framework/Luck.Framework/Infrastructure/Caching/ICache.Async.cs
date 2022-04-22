0402559770a179c5c7045cbd1af8fff6c7e530b4
泽威 王 <1585955375@qq.com>
2022-04-23 00:00:45 +0800

    feat : 添加缓存接口定义

---
+++ src/framework/Luck.Framework/Infrastructure/Caching/ICache.Async.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+
+namespace Luck.Framework.Infrastructure.Caching
+{
+    public partial interface ICache
+    {
+        ValueTask AddAsync<T>(string key, T value, TimeSpan? expiration = null);
+
+        ValueTask TryAddAsync<T>(string key, T value, TimeSpan? expiration = null);
+
+        ValueTask<bool> ExistAsync(string key);
+
+        Task<string> RemoveAsync(string key);
+
+        Task<T> GetAsync<T>(string key);
+
+        Task<T> GetOrAddAsync<T>(string key, TimeSpan? expiration = null);
+
+        Task<T> GetOrUpdateAsync<T>(string key, Func<Task<T>> func, TimeSpan? expiration = null);
+
+        Task<IEnumerable<string>> GetKeysAsync();
+
+        Task ClearAllKeysAsync();
+
+        Task RemoveByPrefixAsync(string prefix);
+    }
+}
---
+++ src/framework/Luck.Framework/Infrastructure/Caching/ICache.cs
@@ -0,0 +1,26 @@
+﻿namespace Luck.Framework.Infrastructure.Caching
+{
+    public partial interface ICache
+    {
+        void Add<T>(string key, T value, TimeSpan? expiration = null);
+
+        bool TryAdd<T>(string key, T value, TimeSpan? expiration = null);
+
+        bool Exist(string key);
+
+        void Remove(string key);
+
+        T Get<T>(string key);
+
+        T GetOrAdd<T>(string key, TimeSpan? expiration = null);
+
+        T GetOrUpdate<T>(string key, Func<Task<T>> func, TimeSpan? expiration = null);
+
+        IEnumerable<string> GetKeys();
+
+        void ClearAllKeys();
+
+        void RemoveByPrefix(string prefix);
+    }
+
+}
