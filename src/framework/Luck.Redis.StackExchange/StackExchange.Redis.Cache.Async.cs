5eae825a5d9fcb3bd49a143f6894e968d44d6d74
泽威 王 <1585955375@qq.com>
2022-04-23 10:44:41 +0800

    feat : 调整目录，添加StackExchange.Redis封装

---
+++ src/framework/Luck.Redis.StackExchange/StackExchange.Redis.Cache.Async.cs
@@ -0,0 +1,77 @@
+﻿using Luck.Framework.Infrastructure.Caching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luck.Redis.StackExchange
+{
+    public partial class RedisCache : ICache
+    {
+        public ValueTask AddAsync<T>(string key, T value, TimeSpan? expiration = null)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task ClearAllKeysAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public ValueTask<bool> ExistAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<T> GetAsync<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public Task<IEnumerable<string>> GetKeysAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public Task<T> GetOrAddAsync<T>(string key, TimeSpan? expiration = null)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public Task<T> GetOrUpdateAsync<T>(string key, Func<Task<T>> func, TimeSpan? expiration = null)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<string> RemoveAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public Task RemoveByPrefixAsync(string prefix)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public ValueTask TryAddAsync<T>(string key, T value, TimeSpan? expiration = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/framework/Luck.Redis.StackExchange/StackExchange.Redis.Cache.cs
@@ -0,0 +1,60 @@
+﻿using Luck.Framework.Infrastructure.Caching;
+
+namespace Luck.Redis.StackExchange
+{
+    public partial class RedisCache : ICache
+    {
+
+        public void Add<T>(string key, T value, TimeSpan? expiration = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearAllKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exist(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetOrAdd<T>(string key, TimeSpan? expiration = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetOrUpdate<T>(string key, Func<Task<T>> func, TimeSpan? expiration = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveByPrefix(string prefix)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryAdd<T>(string key, T value, TimeSpan? expiration = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
