5c5f045b22c2d1556ecebfc0db0bccf942f3d744
泽威 王 <1585955375@qq.com>
2022-04-25 07:33:42 +0800

    feat :解决冲突标记

--- sample/Module.Sample/Program.cs
+++ sample/Module.Sample/Program.cs
@@ -1,6 +1,7 @@
 using Luck.Framework.Infrastructure;
 using MediatR;
 using Module.Sample;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,17 +9,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplication<AppWebModule>();
 
 builder.Services.AddControllers();
-<<<<<<< HEAD
+
 builder.Services.AddMediatR(AssemblyHelper.AllAssemblies);
-=======
-builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+
+
 builder.Services.AddRedis(x =>
 {
     x.Timeout = 1000;
     x.Host = "127.0.0.1:6379";
 
 });
->>>>>>> 23b5e11d2dbbdb471ac868c1dc8ee0b319032187
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
