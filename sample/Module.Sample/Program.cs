1e58a5d4c69b439b1eb7e066a627032d02f96cce
dong <44517714+lurudong@users.noreply.github.com>
2022-04-26 00:21:24 +0800

    faet:

--- sample/Module.Sample/Program.cs
+++ sample/Module.Sample/Program.cs
@@ -8,18 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplication<AppWebModule>();
 
 builder.Services.AddControllers();
-<<<<<<< HEAD
+
 builder.Services.AddMediatR(AssemblyHelper.AllAssemblies);
-=======
-builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 builder.Services.AddRedis(x =>
 {
     x.Timeout = 1000;
     x.Host = "127.0.0.1:6379";
 
 });
->>>>>>> 23b5e11d2dbbdb471ac868c1dc8ee0b319032187
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpContextAccessor();
