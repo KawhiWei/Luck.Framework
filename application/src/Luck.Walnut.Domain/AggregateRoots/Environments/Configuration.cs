b6c9441fb2df644866799152607aa2de08c8998b
泽威 王 <1585955375@qq.com>
2022-04-26 21:28:43 +0800

    feat : 添加配置项的部分字段

--- application/src/Luck.Walnut.Domain/AggregateRoots/Environments/Configuration.cs
+++ application/src/Luck.Walnut.Domain/AggregateRoots/Environments/Configuration.cs
@@ -5,11 +5,31 @@ namespace Luck.Walnut.Domain.AggregateRoots.Environments
     public class Configuration : FullEntity
     { 
     
+        /// <summary>
+        /// 配置项Key
+        /// </summary>
         public string Key { get; private set; } = default!;
 
+        /// <summary>
+        /// 配置项Value
+        /// </summary>
         public string Value { get; private set; } = default!;
 
+        /// <summary>
+        /// 配置项类型
+        /// </summary>
         public string Type { get; private set; } = default!;
+
+        /// <summary>
+        /// 是否公开(其他应用是否可获取)
+        /// </summary>
+        public bool IsOpen { get; private set; } = default!;
+
+        /// <summary>
+        /// 是否发布
+        /// </summary>
+        public bool IsPublish { get; private set; } = default!;
+
     }
 
 }
