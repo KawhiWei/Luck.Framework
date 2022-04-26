fad36380048ef48ff5ceb6a164ba372de4c74c44
泽威 王 <1585955375@qq.com>
2022-04-26 20:44:27 +0800

    feat : 添加配置中心对象

---
+++ application/src/Luck.Walnut.Domain/AggregateRoots/Environments/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using Luck.DDD.Domain;
+
+namespace Luck.Walnut.Domain.AggregateRoots.Environments
+{
+    public class Configuration : FullEntity
+    { 
+    
+        public string Key { get; private set; } = default!;
+
+        public string Value { get; private set; } = default!;
+
+        public string Type { get; private set; } = default!;
+    }
+
+}
---
+++ application/src/Luck.Walnut.Domain/AggregateRoots/Environments/Environment.cs
@@ -0,0 +1,28 @@
+﻿using Luck.DDD.Domain;
+
+namespace Luck.Walnut.Domain.AggregateRoots.Environments
+{
+    /// <summary>
+    /// 环境
+    /// </summary>
+    public class Environment : FullAggregateRoot
+    {
+
+        /// <summary>
+        /// 环境名称
+        /// </summary>
+        public string EnvironmentName { get; private set; } = default!;
+
+        /// <summary>
+        /// 应用Id
+        /// </summary>
+        public string ApplicationId { get;  private set; }=default!;
+        /// <summary>
+        /// 配置项
+        /// </summary>
+        public ICollection<Configuration> Configurations { get; private set; } = default!;
+
+
+    }
+
+}
