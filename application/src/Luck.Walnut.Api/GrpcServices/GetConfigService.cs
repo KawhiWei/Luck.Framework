1350a2a2b7441a08cbac833001e9a37e8a8b46cc
泽威 王 <1585955375@qq.com>
2022-05-01 16:25:21 +0800

    feat : 添加获取配置的grpc protobuf文件

---
+++ application/src/Luck.Walnut.Api/GrpcServices/GetConfigService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+using Luck.Walnut.V1;
+
+namespace Luck.Walnut.Api.GrpcServices
+{
+    public class GetConfigService: GetConfig.GetConfigBase
+    {
+
+
+
+        public override Task<ApplicationConfigResponse> GetAppliactionConfig(ApplicationConfigRequest request, ServerCallContext context)
+        {
+            return base.GetAppliactionConfig(request, context);
+        }
+    }
+}
