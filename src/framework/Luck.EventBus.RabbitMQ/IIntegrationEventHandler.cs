086a9d6c1229057425845fae54c66317179e2942
dong <44517714+lurudong@users.noreply.github.com>
2022-05-08 23:29:04 +0800

    feat:添加集成事件处理器

---
+++ src/framework/Luck.EventBus.RabbitMQ/IIntegrationEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using Luck.Framework.Event;
+
+namespace Luck.EventBus.RabbitMQ
+{
+
+    /// <summary>
+    /// 集成事件处理器
+    /// </summary>
+    public interface IIntegrationEventHandler<in TIntegrationEvent>
+          where TIntegrationEvent : IIntegrationEvent
+    {
+
+        /// <summary>
+        /// 处理器(要不要加Async结尾)
+        /// </summary>
+        /// <param name="event"></param>
+        /// <returns></returns>
+        Task HandleAsync(TIntegrationEvent @event);
+    }
+}
