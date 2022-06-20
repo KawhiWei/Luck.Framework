9ed921ad5de600b56b4deec66b162a33db5f62ab
王泽威 <1585955375@qq.com>
2022-06-20 21:37:38 +0800

    feat : 添加ws修改应用之后主动给客户端发送消息

--- application/src/Luck.Walnut.Api/GrpcServices/LuCatGrpcService.cs
+++ application/src/Luck.Walnut.Api/GrpcServices/LuCatGrpcService.cs
@@ -2,34 +2,52 @@ using System.Collections.Concurrent;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Luck.Walnut.Api.Protos;
+using Luck.Walnut.Application;
 using Luck.Walnut.Application.Environments.Events;
 using MediatR;
 
 namespace Luck.Walnut.Api.GrpcServices;
 
 [AutoMapGrpcService]
-public class LuCatGrpcService : LuCat.LuCatBase, INotificationHandler<AppConfigurationEvent>
+public class LuCatGrpcService : LuCat.LuCatBase
 {
-    private readonly ConcurrentDictionary<string, IServerStreamWriter<BathTheCatResp>> _concurrentQueue =
-        new ConcurrentDictionary<string, IServerStreamWriter<BathTheCatResp>>();
+    private  readonly  ILogger<LuCatGrpcService> _logger;
+    private  readonly  IClientMananger _clientManager;
+
+    public LuCatGrpcService(IClientMananger clientManager,ILogger<LuCatGrpcService> logger)
+    {
+        _clientManager = clientManager;
+        _logger = logger;
+    }
+
     public override async Task BathTheCat(IAsyncStreamReader<BathTheCatReq> requestStream,
         IServerStreamWriter<BathTheCatResp> responseStream, ServerCallContext context)
     {
+        
+        // context.
+        await responseStream.WriteAsync(new BathTheCatResp() { Message = $"铲屎的成功给一只age洗了澡！" });
+        
+        
+        // Task.Run(async () =>
+        // {
+        //     while (true)
+        //     {
+        //         if (_queue.TryTake(out var message))
+        //         {
+        //             await responseStream.WriteAsync(new BathTheCatResp() { Message = $"铲屎的成功给一只{message}洗了澡！" });
+        //         }
+        //
+        //         await Task.Delay(1000);
+        //     }
+        // });
         while (await requestStream.MoveNext())
         {
             //将要洗澡的猫加入队列
             Console.WriteLine($"Cat {requestStream.Current.Id} Enqueue.");
         }
-        _concurrentQueue.TryAdd("aaaa",responseStream);
-    }
-    public async Task Handle(AppConfigurationEvent notification, CancellationToken cancellationToken)
-    {
-        await Task.CompletedTask;
-        Console.WriteLine(notification.Id);
-        //if (_concurrentQueue.TryGetValue(out var responseStream))
-        //{
-          //  await responseStream.WriteAsync(new BathTheCatResp() { Message = $"铲屎的成功给一只{notification.Id}洗了澡！" });
-        //}
+
+        //_clientManager.Update += async (sender, args) => await SendBathTheCatAsync(responseStream, args.Id);
+
     }
 
     public override Task<CountCatResult> Count(Empty request, ServerCallContext context)
@@ -38,5 +56,16 @@ public class LuCatGrpcService : LuCat.LuCatBase, INotificationHandler<AppConfigu
         {
             Count = 80
         });
+    }
+    private async Task SendBathTheCatAsync(IServerStreamWriter<BathTheCatResp> responseStream,string id)
+    {
+        try
+        {
+            await responseStream.WriteAsync(new BathTheCatResp() { Message = $"铲屎的成功给一只{id}洗了澡！" });
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "BathTheCatAsync");       
+        }
     }
 }
--- application/src/Luck.Walnut.Application/ClientManangers/ClientMananger.cs
+++ application/src/Luck.Walnut.Application/ClientManangers/ClientMananger.cs
@@ -1,12 +1,63 @@
+using System.Collections.Concurrent;
+using Luck.Walnut.Application.Environments.Events;
+using MediatR;
+
 namespace Luck.Walnut.Application;
 
-public class ClientMananger:IClientMananger
+public class ClientMananger : IClientMananger
 {
-    
-    
-    
-    public Task RadioClientAsync()
+    public ClientMananger()
     {
-        throw new NotImplementedException();
+        Watching();
     }
+
+    private readonly ConcurrentDictionary<string, List<string>> _clients =
+        new ConcurrentDictionary<string, List<string>>();
+    public event EventHandler<AppConfigurationEvent>? Update;
+
+    public void Add(string appId,string connectionId)
+    {
+        if (_clients.TryGetValue(appId, out var connectionIds))
+        {
+            var newConnectionIds = connectionIds.Select(x => x).ToList();
+            newConnectionIds.Add(connectionId);
+            _clients.TryUpdate(appId, newConnectionIds, connectionIds);
+        }
+        else
+        {
+            connectionIds = new List<string>()
+            {
+                connectionId
+            };
+            _clients.TryAdd(appId, connectionIds);
+        }
+    }
+
+    public  List<string> GetConnectionIds(string appId)
+    {
+        if (_clients.TryGetValue(appId, out var connectionIds))
+        {
+            return connectionIds;
+        }
+        return new List<string>();
+    }
+    private void Watching()
+    {
+        Task.Factory.StartNew(() =>
+        {
+            while (true)
+            {
+                // if (_queue.TryTake(out var message))
+                // {
+                //     Console.WriteLine($"{DateTimeOffset.Now.ToUnixTimeMilliseconds()}------{message}");
+                //     //await responseStream.WriteAsync(new BathTheCatResp() { Message = $"铲屎的成功给一只{message}洗了澡！" });                    
+                // }
+
+                Thread.Sleep(1000);
+                // Task.Delay(1000);
+            }
+        });
+    }
+
+    
 }
