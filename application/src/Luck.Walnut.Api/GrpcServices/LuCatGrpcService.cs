c8c113e8c351ca6811b71855c3be319b666bc941
王泽威 <1585955375@qq.com>
2022-06-21 21:16:53 +0800

    feat : 添加使用loop的方式，grpc服务端主动给客户端发送消息，

--- application/src/Luck.Walnut.Api/GrpcServices/LuCatGrpcService.cs
+++ application/src/Luck.Walnut.Api/GrpcServices/LuCatGrpcService.cs
@@ -3,22 +3,26 @@ using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Luck.Walnut.Api.Protos;
 using Luck.Walnut.Application;
+using Luck.Walnut.Application.Environments;
 using Luck.Walnut.Application.Environments.Events;
 using MediatR;
 
 namespace Luck.Walnut.Api.GrpcServices;
 
 [AutoMapGrpcService]
-public class LuCatGrpcService : LuCat.LuCatBase, INotificationHandler<AppConfigurationEvent>
+public class LuCatGrpcService : LuCat.LuCatBase
 {
-    private  readonly  ILogger<LuCatGrpcService> _logger;
-    private  readonly  IClientMananger _clientManager;
+    private readonly ILogger<LuCatGrpcService> _logger;
+    private readonly IClientMananger _clientManager;
+    private readonly IApplactionClientConcurrentQueue _applactionClientConcurrentQueue;
 
-    public LuCatGrpcService(IClientMananger clientManager,ILogger<LuCatGrpcService> logger)
+    public LuCatGrpcService(IClientMananger clientManager,IApplactionClientConcurrentQueue applactionClientConcurrentQueue, ILogger<LuCatGrpcService> logger)
     {
         _clientManager = clientManager;
+        _applactionClientConcurrentQueue = applactionClientConcurrentQueue;
         _logger = logger;
     }
+
     /// <summary>
     /// 发起链接
     /// </summary>
@@ -28,50 +32,21 @@ public class LuCatGrpcService : LuCat.LuCatBase, INotificationHandler<AppConfigu
     public override async Task BathTheCat(IAsyncStreamReader<BathTheCatReq> requestStream,
         IServerStreamWriter<BathTheCatResp> responseStream, ServerCallContext context)
     {
-        
-        // context.
-        await responseStream.WriteAsync(new BathTheCatResp() { Message = $"铲屎的成功给一只age洗了澡！" });
-        
-        
-        // Task.Run(async () =>
-        // {
-        //     while (true)
-        //     {
-        //         if (_queue.TryTake(out var message))
-        //         {
-        //             await responseStream.WriteAsync(new BathTheCatResp() { Message = $"铲屎的成功给一只{message}洗了澡！" });
-        //         }
-        //
-        //         await Task.Delay(1000);
-        //     }
-        // });
         while (await requestStream.MoveNext())
         {
             //将要洗澡的猫加入队列
             Console.WriteLine($"Cat {requestStream.Current.Id} Enqueue.");
         }
-
-        //_clientManager.Update += async (sender, args) => await SendBathTheCatAsync(responseStream, args.Id);
-
-    }
-
-
-    public async Task Handle(AppConfigurationEvent notification, CancellationToken cancellationToken)
-    {
-        var list = _clientManager.GetConnectionIds(notification.Id);
-        var conn = list.FirstOrDefault(); 
-    }
-    private async Task SendBathTheCatAsync(IServerStreamWriter<BathTheCatResp> responseStream,string id)
-    {
-        try
+        while (true)
         {
-            await responseStream.WriteAsync(new BathTheCatResp() { Message = $"铲屎的成功给一只{id}洗了澡！" });
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "BathTheCatAsync");       
+            if (_applactionClientConcurrentQueue.ConcurrentQueue.TryDequeue(out var str))
+            {
+                await responseStream.WriteAsync(new BathTheCatResp() { Message = $"铲屎的成功给一只{str}洗了澡！" });
+            }
+            await Task.Delay(1000);
         }
     }
+    
     public override Task<CountCatResult> Count(Empty request, ServerCallContext context)
     {
         return Task.FromResult(new CountCatResult()
@@ -79,5 +54,4 @@ public class LuCatGrpcService : LuCat.LuCatBase, INotificationHandler<AppConfigu
             Count = 80
         });
     }
-
 }
--- application/src/Luck.Walnut.Application/ClientManangers/IClientMananger.cs
+++ application/src/Luck.Walnut.Application/ClientManangers/IClientMananger.cs
@@ -1,10 +1,10 @@
+using System.Collections.Concurrent;
 using Luck.Walnut.Application.Environments.Events;
 
 namespace Luck.Walnut.Application;
 
 public interface IClientMananger:ISingletonDependency
 {
-    
     event EventHandler<AppConfigurationEvent> Update;
     void Add(string appId,string connectionId);
     
