f4da869801c094ba63437a20fb262057e99a5f6e
王泽威 <1585955375@qq.com>
2022-08-12 09:50:17 +0800

    feat : 修改文件

--- src/framework/Luck.Framework/build_package.sh
+++ src/framework/Luck.Framework/build_package.sh
@@ -3,4 +3,4 @@ echo "请输入NUGET_API_KEY"
 read NUGET_API_KEY
 dotnet build -c Release
 dotnet pack -c Release
-#dotnet nuget push bin/Release/*.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
+dotnet nuget push bin/Release/*.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
