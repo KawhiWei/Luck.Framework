9a6da9e9b482e67f07f4ccc1418586776a2d73b7
王泽威 <1585955375@qq.com>
2022-08-25 20:00:11 +0800

    feat :

--- src/Luck.Dove.Logging/IDoveLoggerManager.cs
+++
@@ -1,9 +0,0 @@
-namespace Luck.Dove.Logging;
-
-public interface IDoveLoggerManager
-{
-    void Start();
-    void Stop(CancellationToken cancellationToken);
-
-    void Enqueue(string message);
-}
