7da738ee79b8c4fdca31d8aec6bffbb9a641ebf1
王泽威 <1585955375@qq.com>
2022-09-06 09:24:46 +0800

    feat : 修改shell脚本命令

--- src/framework/Luck.Dapper/build_package.sh
+++ src/framework/Luck.Dapper/build_package.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "请输入NUGET_API_KEY"
 read NUGET_API_KEY
+rm bin/Release/*.nupkg
 dotnet build -c Release
 dotnet pack -c Release
 dotnet nuget push bin/Release/*.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
