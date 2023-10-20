751283808c3853d2a20cc15c3fca2e152670c7a3
王泽威 <1585955375@qq.com>
2023-10-20 16:07:37 +0800

    feat ：解决ModuleApplicationBase中的警告

--- src/framework/Luck.AppModule/ModuleApplicationBase.cs
+++ src/framework/Luck.AppModule/ModuleApplicationBase.cs
@@ -13,6 +13,49 @@ namespace Luck.AppModule
         /// <summary>
         /// 
         /// </summary>
+        /// <param name="startupModuleType"></param>
+        /// <param name="services"></param>
+        protected ModuleApplicationBase(Type startupModuleType, IServiceCollection services)
+        {
+            ServiceProvider = null;
+            StartupModuleType = startupModuleType;
+            Services = services;
+            _ = services.AddSingleton<IModuleApplication>(this);
+            _ = services.TryAddObjectAccessor<IServiceProvider>();
+            Source = GetEnabledAllModule(services);
+            Modules = LoadModules;
+        }
+
+        /// <summary>
+        /// 获取所有需要加载的模块
+        /// </summary>
+        /// <exception cref="Exception"></exception>
+        private IReadOnlyList<IAppModule> LoadModules
+        {
+            get
+            {
+                var modules = new List<IAppModule>();
+                var module = Source.FirstOrDefault(o => o.GetType() == StartupModuleType);
+                if (module == null)
+                {
+                    throw new Exception($"类型为“{StartupModuleType.FullName}”的模块实例无法找到");
+                }
+
+                modules.Add(module);
+                var dependedTypes = module.GetDependedTypes();
+                foreach (var dependType in dependedTypes.Where(LuckAppModule.IsAppModule))
+                {
+                    var dependModule = Source.FirstOrDefault(o => o.GetType() == StartupModuleType) ?? throw new($"加载模块{module.GetType().FullName}时无法找到依赖模块{dependType.FullName}");
+                    modules.AddIfNotContains(dependModule);
+                }
+
+                return modules;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public Type StartupModuleType { get; set; }
 
         /// <summary>
@@ -21,9 +64,9 @@ namespace Luck.AppModule
         public IServiceCollection Services { get; set; }
 
         /// <summary>
-        /// 
+        /// IServiceProvider?
         /// </summary>
-        public IServiceProvider ServiceProvider { get; set; }
+        public IServiceProvider? ServiceProvider { get; private set; }
 
         /// <summary>
         /// 模块接口容器
@@ -33,23 +76,7 @@ namespace Luck.AppModule
         /// <summary>
         /// 
         /// </summary>
-        public List<IAppModule> Source { get; protected set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="startupModuleType"></param>
-        /// <param name="services"></param>
-        protected ModuleApplicationBase(Type startupModuleType, IServiceCollection services)
-        {
-            StartupModuleType = startupModuleType;
-            Services = services;
-
-            services.AddSingleton<IModuleApplication>(this);
-            services.TryAddObjectAccessor<IServiceProvider>();
-            Source = this.GetAllModule(services);
-            Modules = this.LoadModules();
-        }
+        public IEnumerable<IAppModule> Source { get; }
 
 
         /// <summary>
@@ -57,11 +84,11 @@ namespace Luck.AppModule
         /// </summary>
         /// <param name="services"></param>
         /// <returns></returns>
-        protected virtual List<IAppModule> GetAllModule(IServiceCollection services)
+        private static IEnumerable<IAppModule> GetEnabledAllModule(IServiceCollection services)
         {
             var findTypes = AssemblyHelper.FindTypes(LuckAppModule.IsAppModule);
-            var modules = findTypes.Select(o => CreateModule(services, o)).Where(o => o.IsNotNull()).Distinct();
-            return modules.ToList();
+            var modules = findTypes.Select(o => CreateModule(services, o)).Where(o => o is not null);
+            return modules.Distinct()!;
         }
 
         /// <summary>
@@ -75,55 +102,17 @@ namespace Luck.AppModule
         }
 
         /// <summary>
-        /// 获取所有需要加载的模块
-        /// </summary>
-        /// <returns></returns>
-        protected virtual IReadOnlyList<IAppModule> LoadModules()
-        {
-            var modules = new List<IAppModule>();
-
-            var module = Source.FirstOrDefault(o => o.GetType() == StartupModuleType);
-            if (module == null)
-            {
-                throw new Exception($"类型为“{StartupModuleType.FullName}”的模块实例无法找到");
-            }
-            modules.Add(module);
-            var dependedTypes = module.GetDependedTypes();
-            foreach (var dependType in dependedTypes.Where(LuckAppModule.IsAppModule))
-            {
-                var dependModule = Source.Find(m => m.GetType() == dependType);
-                if (dependModule == null)
-                {
-                    throw new Exception($"加载模块{module.GetType().FullName}时无法找到依赖模块{dependType.FullName}");
-                }
-                modules.AddIfNotContains(dependModule);
-            }
-            return modules;
-        }
-
-        /// <summary>
         /// 创建模块
         /// </summary>
         /// <param name="services"></param>
         /// <param name="moduleType"></param>
         /// <exception cref="ArgumentNullException"></exception>
         /// <returns></returns>
-        private static IAppModule CreateModule(IServiceCollection services, Type moduleType)
+        private static IAppModule? CreateModule(IServiceCollection services, Type moduleType)
         {
-            var invoke = Expression.Lambda(Expression.New(moduleType)).Compile().DynamicInvoke();
-
-
-            if (invoke == null)
-            {
-                throw new ArgumentNullException(nameof(invoke));
-            }
-            var module = (IAppModule)invoke;
-
-
-            if (module == null)
-                throw new ArgumentNullException(nameof(module));
-
-            if (module.Enable == false)
+            var module = Expression.Lambda(Expression.New(moduleType)).Compile().DynamicInvoke() as IAppModule;
+            ArgumentNullException.ThrowIfNull(module, nameof(moduleType));
+            if (!module.Enable)
             {
                 return null;
             }
@@ -136,7 +125,7 @@ namespace Luck.AppModule
         /// </summary>
         public virtual void Dispose()
         {
-            Source?.Clear();
+            GC.SuppressFinalize(this);
         }
     }
 }
