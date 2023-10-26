41c8abff193109898226eab183e7e2a83547e102
kawhi <1585955375@qq.com>
2023-10-26 18:50:10 +0800

    feature 1、EFCore添加内存数据库使用

---
+++ src/framework/Luck.EntityFrameworkCore.MemoryDataBase/ServiceCollectionExtension.cs
@@ -0,0 +1,13 @@
+﻿using Luck.EntityFrameworkCore.DbContextDrivenProvides;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Luck.EntityFrameworkCore.MemoryDatabase;
+
+public class ServiceCollectionExtension
+{
+    public virtual IServiceCollection AddMemoryDriven(IServiceCollection services)
+    {
+        services.AddSingleton<IDbContextDrivenProvider, MemoryDrivenProvider>();
+        return services;
+    }
+}
